61b10da5e86eabf54d73659057b9446b020bd1ae
ssndrss <sandra.sini@epsi.fr>
2023-10-15 23:43:41 +0200

    portfolio v2

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ gsap.to("#card1", {
     duration: 0.5,
     scrollTrigger: "#card1",
     start: "top center",
-    markers: true,
     scrub: true
 })
 
@@ -15,7 +14,6 @@ gsap.to("#card2", {
     delay: 0.5,
     scrollTrigger: "#card2",
     start: "top center",
-    markers: true,
     scrub: true
 })
 
@@ -25,6 +23,91 @@ gsap.to("#card3", {
     delay: 1,
     scrollTrigger: "#card3",
     start: "top center",
-    markers: true,
+    scrub: true
+})
+
+gsap.to("#line1", {
+    width: "70%",
+    duration: 0.8,
+    scrollTrigger: "#line1",
+    start: "top 70%",
+    scrub: true
+})
+
+gsap.to("#line2", {
+    width: "70%",
+    duration: 0.8,
+    scrollTrigger: "#line2",
+    start: "top 70%",
+    scrub: true
+})
+
+gsap.to("#image1", {
+    scale: 1,
+    duration: 0.5,
+    scrollTrigger: "#image1",
+    start: "top 70%",
+    scrub: true
+})
+
+gsap.to("#image2", {
+    scale: 1,
+    duration: 0.5,
+    delay: 0.5,
+    scrollTrigger: "#image2",
+    start: "top 70%",
+    scrub: true
+})
+
+gsap.to("#image3", {
+    scale: 1,
+    duration: 0.5,
+    delay: 1,
+    scrollTrigger: "#image3",
+    start: "top 70%",
+    scrub: true
+})
+
+gsap.to("#image4", {
+    scale: 1,
+    duration: 0.5,
+    delay: 1.5,
+    scrollTrigger: "#image4",
+    start: "top 70%",
+    scrub: true
+})
+
+gsap.to("#image5", {
+    scale: 1,
+    duration: 0.5,
+    scrollTrigger: "#image5",
+    start: "center 40%",
+    scrub: true
+})
+
+gsap.to("#image6", {
+    scale: 1,
+    duration: 0.5,
+    delay: 0.5,
+    scrollTrigger: "#image6",
+    start: "center 40%",
+    scrub: true
+})
+
+gsap.to("#image7", {
+    scale: 1,
+    duration: 0.5,
+    delay: 1,
+    scrollTrigger: "#image7",
+    start: "center 40%",
+    scrub: true
+})
+
+gsap.to("#image8", {
+    scale: 1,
+    duration: 0.5,
+    scrollTrigger: "#image8",
+    start: "center 40%",
+    end: "bottom top",
     scrub: true
 })
